08fc4c7de0728a884ccf1a7c672e1a4dc3dceaab
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-29 18:26:42 -0400

    fetch the latitude and longitude

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const request = require('request');
 
 const { fetchMyIP } = require('./iss');
 
+
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -10,4 +11,15 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('99.251.54.96', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
