13b99cf517e746cbf0067b7887a0e296d706fdac
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-29 20:56:11 -0400

    Implemented the function

--- index.js
+++ index.js
@@ -36,3 +36,23 @@ fetchISSFlyOverTimes(exampleCoords, (error, pass) => {
 
   console.log('It worked! Returned flyover times:' , pass);
 });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
